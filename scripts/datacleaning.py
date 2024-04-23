2de96d7c3d902279abf42ed020fd759e9f39ec8a
ithierda <113621442+ithierda@users.noreply.github.com>
2024-04-23 21:27:51 +0200

    Update datacleaning.py

--- scripts/datacleaning.py
+++ scripts/datacleaning.py
@@ -1,1 +1,13 @@
 #data cleaning script
+def moyenne_nombre_employes(text):
+    # Utiliser une expression régulière pour trouver tous les nombres dans le texte
+    nombres = re.findall(r'\d+', text)
+    
+    # Convertir les résultats de texte à entier
+    nombres = [int(n) for n in nombres]
+    
+    # Calculer la moyenne si on a exactement deux nombres
+    if len(nombres) == 2:
+        return sum(nombres) / 2
+    else:
+        return nombres[0]
